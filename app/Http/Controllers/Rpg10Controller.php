3404da3aa5068c61101eae6f39e3305cabada865
魏國安 <qaz898970@gmail.com>
2025-06-19 15:14:20 +0800

    20250619修改專員金紙報表

--- app/Http/Controllers/Rpg10Controller.php
+++ app/Http/Controllers/Rpg10Controller.php
@@ -36,7 +36,7 @@ class Rpg10Controller extends Controller
                     ->where('sale_data.type_list','dispatch')
                     ->where('sale_data.status','9')
                     ->where('product.commission', '0')
-                    ->where('users.status', '0')
+                    // ->where('users.status', '0')
                     ->whereIn('users.job_id',[1,3,5])
                     ->whereNotNull('sale_gdpaper.gdpaper_id')
                     // ->whereNotNull('sale_data.plan_id')
