5fb4c11ba934b20ea3bf322657ee319336ab975c
魏國安 <qaz898970@gmail.com>
2025-06-23 16:10:36 +0800

    來源叫件新增總件數

--- app/Http/Controllers/Rpg14Controller.php
+++ app/Http/Controllers/Rpg14Controller.php
@@ -9,62 +9,102 @@ use App\Models\Sale;
 use App\Models\Plan;
 use App\Models\SaleSource;
 use Illuminate\Support\Facades\Redis;
+use Intervention\Image\Colors\Rgb\Channels\Red;
+use Illuminate\Support\Facades\DB;
 
 class Rpg14Controller extends Controller
 {
     public function rpg14(Request $request)
     {
         $years = range(Carbon::now()->year, 2022);
-            if (isset($request)) {
-                $search_year = $request->year;
-                $search_month = $request->month;
-                $firstDay = Carbon::createFromDate($search_year , $search_month,1)->firstOfMonth();
-                $lastDay = Carbon::createFromDate($search_year , $search_month,1)->lastOfMonth();
-            } else {
-                $firstDay = Carbon::now()->firstOfMonth();
-                $lastDay = Carbon::now()->lastOfMonth();
+        if (isset($request)) {
+            $search_year = $request->year;
+            $search_month = $request->month;
+            $firstDay = Carbon::createFromDate($search_year, $search_month, 1)->firstOfMonth();
+            $lastDay = Carbon::createFromDate($search_year, $search_month, 1)->lastOfMonth();
+            $month = Carbon::createFromDate($search_year, $search_month, 1)->format('Y-m');
+        } else {
+            $firstDay = Carbon::now()->firstOfMonth();
+            $lastDay = Carbon::now()->lastOfMonth();
+            $month = Carbon::createFromDate($firstDay, $lastDay, 1)->format('Y-m');
+        }
+        $periods = CarbonPeriod::create($firstDay, $lastDay);
+
+        //單純有拜訪的
+        $visit_sources = SaleSource::where('status', 'up')->whereIn('code',['H','Salon','B','G','dogpark','other'])->orderby('id')->get();
+
+
+        $sources = SaleSource::where('status', 'up')->orderby('id')->get();
+        foreach ($periods as $period) {
+            foreach ($sources as $source) {
+                $datas[$period->format("Y-m-d")][$source->code]['name'] = $source->name;
+                $datas[$period->format("Y-m-d")][$source->code]['count'] = 0;
+                $sums[$source->code]['count'] = 0;
+                $sums[$source->code]['count'] += $datas[$period->format("Y-m-d")][$source->code]['count'];
             }
-            $periods = CarbonPeriod::create($firstDay, $lastDay);
-            $sources = SaleSource::where('status', 'up')->orderby('id')->get();
-            foreach ($periods as $period) {
-                foreach ($sources as $source) {
-                    $datas[$period->format("Y-m-d")][$source->code]['name'] = $source->name;
-                    $datas[$period->format("Y-m-d")][$source->code]['count'] = 0;
-                    $sums[$source->code]['count'] = 0;
-                    $sums[$source->code]['count'] += $datas[$period->format("Y-m-d")][$source->code]['count'];
+            $sales = Sale::where('sale_date', $period->format("Y-m-d"))->where('status', '9')->whereIn('pay_id', ['A', 'C'])->get();
+            foreach ($sales as $sale) {
+                if (!isset($datas[$period->format("Y-m-d")][$sale->type]['count'])) {
+                    $datas[$period->format("Y-m-d")][$sale->type]['count'] = 0;
                 }
-                $sales = Sale::where('sale_date', $period->format("Y-m-d"))->where('status', '9')->whereIn('pay_id', ['A', 'C'])->get();
-                foreach ($sales as $sale) {
-                    if (!isset($datas[$period->format("Y-m-d")][$sale->type]['count'])) {
-                        $datas[$period->format("Y-m-d")][$sale->type]['count'] = 0;
-                    }
-                    $datas[$period->format("Y-m-d")][$sale->type]['count']++;
-                }
+                $datas[$period->format("Y-m-d")][$sale->type]['count']++;
             }
-            // dd($datas);
-            foreach ($periods as $period) {
-                foreach ($sources as $source) {
-                    $sums[$source->code]['count'] += $datas[$period->format("Y-m-d")][$source->code]['count'];
-                }
+        }
+        // dd($datas);
+        foreach ($periods as $period) {
+            foreach ($sources as $source) {
+                $sums[$source->code]['count'] += $datas[$period->format("Y-m-d")][$source->code]['count'];
             }
-            // dd($datas);
+        }
+        // dd($datas);
         return view('rpg14.index')->with('datas', $datas)
-                                    ->with('sums', $sums)
-                                    ->with('sources', $sources)
-                                    ->with('years', $years)
-                                    ->with('request', $request);
+            ->with('sums', $sums)
+            ->with('sources', $sources)
+            ->with('years', $years)
+            ->with('request', $request)
+            ->with('month', $month);
     }
 
-    public function detail(Request $request , $date , $source_code)
+    public function detail(Request $request, $date, $source_code)
     {
         $sources = SaleSource::where('status', 'up')->orderby('id')->get();
-        foreach($sources as $source){
-            $source_name[$source->code] = $source->name; 
+        foreach ($sources as $source) {
+            $source_name[$source->code] = $source->name;
         }
-        $datas = Sale::where('sale_date',$date)->where('status', '9')->whereIn('pay_id', ['A', 'C'])->where('type',$source_code)->get();
-        return view('rpg14.detail')->with('datas',$datas)
-                                   ->with('source_name',$source_name)
-                                   ->with('date',$date)
-                                   ->with('source_code',$source_code);
+        $datas = Sale::where('sale_date', $date)->where('status', '9')->whereIn('pay_id', ['A', 'C'])->where('type', $source_code)->get();
+        return view('rpg14.detail')->with('datas', $datas)
+            ->with('source_name', $source_name)
+            ->with('date', $date)
+            ->with('source_code', $source_code);
+    }
+
+    public function month_detail(Request $request, $month, $source_code)
+    {
+        $sources = SaleSource::where('status', 'up')->orderby('id')->get();
+        foreach ($sources as $source) {
+            $source_name[$source->code] = $source->name;
+        }
+        $firstDay = Carbon::createFromDate($month, 1)->firstOfMonth();
+        $lastDay = Carbon::createFromDate($month, 1)->lastOfMonth();
+        $datas = Sale::whereBetween('sale_data.sale_date', [$firstDay, $lastDay])
+            ->join('sale_company_commission', 'sale_company_commission.sale_id', '=', 'sale_data.id')
+            ->join('customer', 'customer.id', '=', 'sale_company_commission.company_id')
+            ->whereBetween('sale_data.sale_date', [$firstDay, $lastDay])
+            ->where('sale_data.status', '9')
+            ->whereIn('sale_data.pay_id', ['A', 'C'])
+            ->where('sale_data.type', $source_code)
+            ->select(
+                'sale_company_commission.company_id',
+                'customer.name',
+                DB::raw('count(*) as total')
+            )
+            ->groupBy('sale_company_commission.company_id')
+            ->orderByDesc('total')
+            ->get();
+        
+        return view('rpg14.month_detail')->with('datas', $datas)
+            ->with('source_name', $source_name)
+            ->with('month', $month)
+            ->with('source_code', $source_code);
     }
 }
---
+++ resources/views/rpg14/month_detail.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.vertical', ["page_title"=> "每月來源報表"])
+
+@section('content')
+<!-- Start Content-->
+<div class="container-fluid">
+
+    <!-- start page title -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Huaxixiang</a></li>
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">報表管理</a></li>
+                        <li class="breadcrumb-item active">每月來源報表</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">{{ $month."【".$source_name[$source_code]."】來源" }}</h4>
+            </div>
+        </div>
+    </div>
+    <!-- end page title -->
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive ">
+                            <table class="table table-centered table-nowrap table-hover mb-0 mt-2">
+                                <thead>
+                                    <tr align="center">
+                                        <td>No</td>
+                                        <td>名稱</td>
+                                        <td>件數</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($datas as $key=>$data)
+                                        <tr align="center">
+                                            <td>{{ $key+1 }}</td>
+                                            <td>{{ $data->name }}</td>
+                                            <td>{{ $data->total }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            <br>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="text-center mb-3">
+                                        <button type="reset" class="btn btn-secondary waves-effect waves-light m-1" onclick="history.go(-1)">回上一頁</button>
+                                    </div>
+                                </div> <!-- end col -->
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+                    
+
+</div> <!-- container -->
+@endsection
