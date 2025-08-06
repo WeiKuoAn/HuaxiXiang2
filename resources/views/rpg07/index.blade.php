acf9cf6c4bcf7ee72f35cc70073db3c9418d873c
魏國安 <qaz898970@gmail.com>
2025-08-06 18:10:59 +0800

    調整銷售數據顯示邏輯，新增功德筆數顯示，並優化資料迴圈結構，提升用戶在查看銷售與功德記錄時的清晰度與完整性。

--- resources/views/rpg07/index.blade.php
+++ resources/views/rpg07/index.blade.php
@@ -63,7 +63,13 @@
                             @if (Auth::user()->level != 2)
                                 <div class="col-auto">
                                     <div class="text-lg-end my-1 my-lg-0">
-                                        <h3><span class="text-danger">共{{ number_format($kgs,2) }}公斤，共計{{ number_format($total_price) }}元</span></h3>
+                                        <h3><span class="text-danger">共{{ number_format($kgs,2) }}公斤，共計{{ number_format($total_price) }}元，功德{{ $total_merits }}筆</span></h3>
+                                    </div>
+                                </div><!-- end col-->
+                            @else
+                                <div class="col-auto">
+                                    <div class="text-lg-end my-1 my-lg-0">
+                                        <h3><span class="text-danger">共{{ number_format($kgs,2) }}公斤，功德{{ $total_merits }}筆</span></h3>
                                     </div>
                                 </div><!-- end col-->
                             @endif
@@ -100,104 +106,140 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($datas as $data)
-                                        <tr>
-                                            <td align="center">{{ $data->sale_date }}</td>
-                                            <td align="center">
-                                                @if (isset($data->customer_id))
-                                                    @if (isset($data->cust_name))
-                                                        {{ $data->cust_name->name }}
-                                                    @else
-                                                        {{ $data->customer_id }}
-                                                    @endif
-                                                @endif
-                                            </td>
-                                            <td align="center">
-                                                @if (isset($data->pet_name))
-                                                    {{ $data->pet_name }}
-                                                @endif
-                                            </td>
-                                            <td align="center">
-                                                {{ $data->kg }}
-                                            </td>
-                                            <td align="center">
-                                                {{ $data->variety }}
-                                            </td>
-                                            <td align="center">
-                                                @if ($data->pay_id == 'E')
-                                                    {{ number_format($data->pay_price) }}
-                                                @else
-                                                    {{ number_format($data->plan_price) }}
-                                                @endif
-                                            </td>
-                                            <td align="center">
-                                                @if (isset($data->type))
-                                                    {{ $data->source_type->name }}
-                                                @endif
-                                            </td>
-                                            <td align="center">
-                                                @if (isset($data->plan_id))
-                                                    @if (isset($data->plan_name))
-                                                        {{ $data->plan_name->name }}
-                                                    @else
-                                                        {{ $data->plan_id }}
-                                                    @endif
-                                                @endif
-                                            </td>
-                                            <td>
-                                                @foreach ($data->gdpapers as $gdpaper)
-                                                    @if (isset($gdpaper->gdpaper_id))
-                                                        @if ($data->plan_id != '4')
-                                                            {{ $gdpaper->gdpaper_name->name }}-{{ $gdpaper->gdpaper_num }}份<br>
+                                    @foreach ($datas as $item)
+                                        @if($item['type'] === 'sale')
+                                            <tr>
+                                                <td align="center">{{ $item['data']->sale_date }}</td>
+                                                <td align="center">
+                                                    @if (isset($item['data']->customer_id))
+                                                        @if (isset($item['data']->cust_name))
+                                                            {{ $item['data']->cust_name->name }}
                                                         @else
-                                                            {{ $gdpaper->gdpaper_name->name }}{{ number_format($gdpaper->gdpaper_num) }}份<br>
+                                                            {{ $item['data']->customer_id }}
                                                         @endif
-                                                    @else
-                                                        無
                                                     @endif
-                                                @endforeach
-                                            </td>
-                                            <td>
-                                                @if (isset($data->before_prom_id))
-                                                    @if (isset($data->PromA_name))
-                                                        {{ $data->PromA_name->name }}-{{ number_format($data->before_prom_price) }}
-                                                    @else
-                                                        {{ $data->before_prom_id }}
+                                                </td>
+                                                <td align="center">
+                                                    @if (isset($item['data']->pet_name))
+                                                        {{ $item['data']->pet_name }}
                                                     @endif
-                                                @endif
-                                                @foreach ($data->proms as $prom)
-                                                    @if ($prom->prom_type == 'A')
-                                                        @if (isset($prom->prom_id))
-                                                            {{ $prom->prom_name->name }}-{{ number_format($prom->prom_total) }}<br>
+                                                </td>
+                                                <td align="center">
+                                                    {{ $item['data']->kg }}
+                                                </td>
+                                                <td align="center">
+                                                    {{ $item['data']->variety }}
+                                                </td>
+                                                <td align="center">
+                                                    @if ($item['data']->pay_id == 'E')
+                                                        {{ number_format($item['data']->pay_price) }}
+                                                    @else
+                                                        {{ number_format($item['data']->plan_price) }}
+                                                    @endif
+                                                </td>
+                                                <td align="center">
+                                                    @if (isset($item['data']->type))
+                                                        {{ $item['data']->source_type->name }}
+                                                    @endif
+                                                </td>
+                                                <td align="center">
+                                                    @if (isset($item['data']->plan_id))
+                                                        @if (isset($item['data']->plan_name))
+                                                            {{ $item['data']->plan_name->name }}
+                                                        @else
+                                                            {{ $item['data']->plan_id }}
+                                                        @endif
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    @foreach ($item['data']->gdpapers as $gdpaper)
+                                                        @if (isset($gdpaper->gdpaper_id))
+                                                            @if ($item['data']->plan_id != '4')
+                                                                {{ $gdpaper->gdpaper_name->name }}-{{ $gdpaper->gdpaper_num }}份<br>
+                                                            @else
+                                                                {{ $gdpaper->gdpaper_name->name }}{{ number_format($gdpaper->gdpaper_num) }}份<br>
+                                                            @endif
                                                         @else
                                                             無
                                                         @endif
-                                                    @endif
-                                                @endforeach
-                                            </td>
-                                            <td align="center">
-                                                @foreach ($data->proms as $prom)
-                                                    @if ($prom->prom_type == 'B')
-                                                        @if (isset($prom->prom_id))
-                                                            {{ $prom->prom_name->name }}<br>
+                                                    @endforeach
+                                                </td>
+                                                <td>
+                                                    @if (isset($item['data']->before_prom_id))
+                                                        @if (isset($item['data']->PromA_name))
+                                                            {{ $item['data']->PromA_name->name }}-{{ number_format($item['data']->before_prom_price) }}
                                                         @else
-                                                            無
+                                                            {{ $item['data']->before_prom_id }}
                                                         @endif
                                                     @endif
-                                                @endforeach
-                                            </td>
-                                            <td align="center">
-                                                @if (isset($data->pay_id))
-                                                    {{ $data->pay_type() }}
+                                                    @foreach ($item['data']->proms as $prom)
+                                                        @if ($prom->prom_type == 'A')
+                                                            @if (isset($prom->prom_id))
+                                                                {{ $prom->prom_name->name }}-{{ number_format($prom->prom_total) }}<br>
+                                                            @else
+                                                                無
+                                                            @endif
+                                                        @endif
+                                                    @endforeach
+                                                </td>
+                                                <td align="center">
+                                                    @foreach ($item['data']->proms as $prom)
+                                                        @if ($prom->prom_type == 'B')
+                                                            @if (isset($prom->prom_id))
+                                                                {{ $prom->prom_name->name }}<br>
+                                                            @else
+                                                                無
+                                                            @endif
+                                                        @endif
+                                                    @endforeach
+                                                </td>
+                                                <td align="center">
+                                                    @if (isset($item['data']->pay_id))
+                                                        {{ $item['data']->pay_type() }}
+                                                    @endif
+                                                </td>
+                                                @if (Auth::user()->level == 0)
+                                                    <td align="center">{{ number_format($item['data']->pay_price) }}</td>
                                                 @endif
-                                            </td>
-                                            @if (Auth::user()->level == 0)
-                                                <td align="center">{{ number_format($data->pay_price) }}</td>
-                                            @endif
-                                            <td class="wrap-text">
-                                                {{ $data->comm }}
-                                            </td>
-                                        </tr>
+                                                <td class="wrap-text">
+                                                    {{ $item['data']->comm }}
+                                                </td>
+                                            </tr>
+                                        @elseif($item['type'] === 'merit_only')
+                                            @php
+                                                $merit = $item['merits']->first();
+                                                $user_name = $merit && $merit->user_data ? $merit->user_data->name : '功德記錄';
+                                                // Debug: 輸出功德記錄資訊
+                                                \Log::info('功德記錄顯示:', [
+                                                    'date' => $item['date'],
+                                                    'merit_exists' => $merit ? 'yes' : 'no',
+                                                    'user_data_exists' => $merit && $merit->user_data ? 'yes' : 'no',
+                                                    'user_name' => $user_name,
+                                                    'kg' => $merit ? $merit->kg : 'N/A',
+                                                    'variety' => $merit ? $merit->variety : 'N/A'
+                                                ]);
+                                            @endphp
+                                            <tr class="table-secondary">
+                                                <td align="center">{{ $item['date'] }}</td>
+                                                <td align="center">{{ $user_name }}</td>
+                                                <td align="center">-</td>
+                                                <td align="center">{{ $merit ? ($merit->kg ?? 0) : 0 }}</td>
+                                                <td align="center">{{ $merit ? ($merit->variety ?? '功德') : '功德' }}</td>
+                                                <td align="center">-</td>
+                                                <td align="center">功德</td>
+                                                <td align="center">-</td>
+                                                <td align="center">-</td>
+                                                <td align="center">-</td>
+                                                <td align="center">-</td>
+                                                <td align="center">-</td>
+                                                @if (Auth::user()->level == 0)
+                                                    <td align="center">-</td>
+                                                @endif
+                                                <td class="wrap-text">
+                                                    <span class="text-muted">功德件</span>
+                                                </td>
+                                            </tr>
+                                        @endif
                                     @endforeach
                                 </tbody>
                             </table>
