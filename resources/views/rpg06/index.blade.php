6090b4524acd8d1f69a58206810f3e7d5e193ab2
魏國安 <qaz898970@gmail.com>
2025-09-13 13:55:54 +0800

    新增備註欄位於RPG06視圖中，以顯示相關評論，提升資料的完整性與可讀性。

--- resources/views/rpg06/index.blade.php
+++ resources/views/rpg06/index.blade.php
@@ -71,6 +71,7 @@
                                     <th scope="col">寶貝名稱</th>
                                     <th scope="col">客戶電話</th>
                                     <th scope="col">法會費用</th>
+                                    <th scope="col">備註</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -81,6 +82,7 @@
                                         <td>{{ $data->sale_data->pet_name }}</td>
                                         <td>{{ $data->sale_data->cust_name->mobile }}</td>
                                         <td>{{  number_format($data->prom_total) }}</td>
+                                        <td>{{ $data->comment }}</td>
                                     </tr>
                                 @endforeach
                             </tbody>
