6e629fd2d345e7a891f6f33bbb71f61431996419
魏國安 <qaz898970@gmail.com>
2025-11-25 11:27:10 +0800

    Update hospital visit dropdown options to improve navigation; replace 'edit' link with 'create visit' and adjust icons for clarity.

--- resources/views/visit/hospitals.blade.php
+++ resources/views/visit/hospitals.blade.php
@@ -253,14 +253,6 @@
                                                         data-bs-toggle="dropdown" aria-expanded="false">動作 <i
                                                             class="mdi mdi-arrow-down-drop-circle"></i></a>
                                                     <div class="dropdown-menu dropdown-menu-end">
-                                                        <a class="dropdown-item"
-                                                            href="{{ route('visit.company.edit', $data->id) }}"
-                                                            target="_blank"><i
-                                                                class="mdi mdi-pencil me-2 text-muted font-18 vertical-middle"></i>編輯</a>
-                                                        <a class="dropdown-item"
-                                                            href="{{ route('visit.source.sale', $data->id) }}"
-                                                            target="_blank"><i
-                                                                class="mdi mdi-clipboard-text-search me-2 font-18 text-muted vertical-middle"></i>叫件紀錄</a>
                                                         <a class="dropdown-item" href="{{ route('visits', $data->id) }}"
                                                             target="_blank"><i
                                                                 class="mdi mdi-file-document me-2 font-18 text-muted vertical-middle"></i>查看拜訪</a>
@@ -268,6 +260,14 @@
                                                             href="{{ route('visit.create', $data->id) }}"
                                                             target="_blank"><i
                                                                 class="mdi mdi-text-box-plus-outline me-2 text-muted font-18 vertical-middle"></i>新增拜訪</a>
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('visit.source.sale', $data->id) }}"
+                                                            target="_blank"><i
+                                                                class="mdi mdi-clipboard-text-search me-2 font-18 text-muted vertical-middle"></i>叫件紀錄</a>
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('visit.company.edit', $data->id) }}"
+                                                            target="_blank"><i
+                                                                class="mdi mdi-pencil me-2 text-muted font-18 vertical-middle"></i>編輯資料</a>
                                                     </div>
                                                 </div>
                                             </td>
